e31cf3124a0ecb8d340d03aaf339d8c0ac84ed95
™gislpzbyu™ <gislpz@byui.edu>
2025-03-21 18:16:42 -0600

    Deliver Registration View

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,19 +1,35 @@
 // controllers/accountController.js
-const utilities = require("../utilities")
+const utilities = require("../utilities");
 
 /* ****************************************
  *  Deliver login view
  * *************************************** */
 async function buildLogin(req, res, next) {
   try {
-    let nav = await utilities.getNav()
+    let nav = await utilities.getNav();
     res.render("account/login", {
       title: "Login",
       nav,
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-module.exports = { buildLogin }
+/* ****************************************
+ *  Deliver registration view
+ * *************************************** */
+async function buildRegister(req, res, next) {
+  try {
+    let nav = await utilities.getNav();
+    res.render("account/register", {
+      title: "Register",
+      nav,
+      errors: null, // In case we want to show error messages later
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { buildLogin, buildRegister };
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,16 +1,19 @@
 // routes/accountRoute.js
-const express = require("express")
-const router = new express.Router()
-const utilities = require("../utilities/")
-const accountController = require("../controllers/accountController")
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities/");
+const accountController = require("../controllers/accountController");
 
 // Route for the login view
-router.get("/login", accountController.buildLogin)
+router.get("/login", accountController.buildLogin);
+
+// Route for the registration view
+router.get("/register", accountController.buildRegister);
 
 // Error handling middleware
 router.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send("Something went wrong!")
-})
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
-module.exports = router
+module.exports = router;
